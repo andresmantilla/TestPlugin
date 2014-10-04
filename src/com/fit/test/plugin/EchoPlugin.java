99cf7099d7d86ce67dff24223f8d558bedc75a9b
andresmantilla <andres.mantilla@fitideas.co>
2014-10-04 13:25:56 -0500

    1325

--- src/com/fit/test/plugin/EchoPlugin.java
+++ src/com/fit/test/plugin/EchoPlugin.java
@@ -1,6 +1,9 @@
 package com.fit.test.plugin;
-import org.apache.cordova.CordovaPlugin;
+
+import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -9,6 +12,10 @@ import org.json.JSONObject;
 
 public class EchoPlugin extends CordovaPlugin {
 	
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+    
 	@Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("echo")) {
--- www/echo.js
+++ www/echo.js
@@ -1,7 +1,18 @@
-var echoPlugin = {
-	echo: function(str, callback) {
-        cordova.exec(callback, function(err) {
-            callback('Nothing to echo.');
-        }, "Echo", "echo", [str]);
-	}
+var exec = argscheck = require('cordova/argscheck'),
+	channel = require('cordova/channel'),
+	utils = require('cordova/utils'),
+	require('cordova/exec'),
+    cordova = require('cordova');
+
+function Echo () {
+	
+	
 }
+
+Echo.prototype.echo = (str, callback) {
+    exec(callback, function(err) {
+        callback('Nothing to echo.');
+    }, "Echo", "echo", [str]);
+}
+
+module.exports = new Echo();

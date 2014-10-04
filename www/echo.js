8ef0479f81b7c5ef08d98a3687fa50615b453c3e
andresmantilla <andres.mantilla@fitideas.co>
2014-10-04 13:39:04 -0500

    1338

--- www/echo.js
+++ www/echo.js
@@ -1,12 +1,10 @@
-var exec = argscheck = require('cordova/argscheck'),
-	channel = require('cordova/channel'),
-	utils = require('cordova/utils'),
-	require = ('cordova/exec'),
+var argscheck = require('cordova/argscheck'),
+    channel = require('cordova/channel'),
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec'),
     cordova = require('cordova');
 
 function Echo () {
-	
-	
 }
 
 Echo.prototype.echo = function (str, callback) {
